da2daa781dc1616745cbc058a385a9a32e58e904
IvanKrutik <ivan.krutik@gmail.com>
2023-08-28 20:37:40 +0400

    10-conditional-rendering

---
+++ 10-conditional-rendering/src/components/PetInfo.js
@@ -0,0 +1,10 @@
+function PetInfo(props) {
+  const { animal, age, hasPet } = props
+  const text = hasPet
+    ? `My ${animal} is ${age} years old`
+    : "I don't have an animal"
+
+  return <h1>{text}</h1>
+}
+
+export default PetInfo
