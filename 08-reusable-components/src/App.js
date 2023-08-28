44f77400f0a7f4ed1426c1ba5e9a891f70194a0f
IvanKrutik <ivan.krutik@gmail.com>
2023-08-28 17:32:49 +0400

    08-reusable-components

---
+++ 08-reusable-components/src/App.js
@@ -0,0 +1,17 @@
+import './App.css'
+import MyComponent from './Components/MyComponent'
+import OtherComponent from './Components/OtherComponent'
+
+function App() {
+  return (
+    <div className="App">
+      <MyComponent />
+      <OtherComponent />
+      <MyComponent />
+      <OtherComponent />
+      <MyComponent />
+    </div>
+  )
+}
+
+export default App
